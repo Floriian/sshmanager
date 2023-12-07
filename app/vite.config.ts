39f8164ec1faf1a4aa0684043f692737c7837bbc
Floriian <lovasifloriian@outlook.hu>
2023-12-07 22:02:04 +0100

    fix path alias.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,9 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@app": path.resolve(__dirname, "/src/components"),
-      "@features": path.resolve(__dirname, "/src/features"),
-      "@components": path.resolve(__dirname, "/src/components"),
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@components": path.resolve(__dirname, "./src/components"),
     },
   },
 });
