10472664354ba08c75cc0a4e6824dcaa01719021
Floriian <lovasifloriian@outlook.hu>
2023-12-07 20:36:24 +0100

    update create-server.dto

--- api/src/servers/dto/create-server.dto.ts
+++ api/src/servers/dto/create-server.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateServerDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   address: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 }
